93e97d6d0ac3f82a99fd9803bca7d7460c671cb3
anithishkumaran <129111432+anithishkumaran@users.noreply.github.com>
2023-04-23 19:51:50 +0530

    Update cLeetCodeSolution1.c

--- cLeetCodeSolution1.c
+++ cLeetCodeSolution1.c
@@ -1,6 +1,18 @@
-#include <stdio.h>
-int main(){
-    printf("nithish is badd");
-return 0;
-}
+//Search Insert Position//
+int searchInsert(int* nums, int numsSize, int target) {
+    int left = 0;
+    int right = numsSize - 1;
 
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return left;
+}
